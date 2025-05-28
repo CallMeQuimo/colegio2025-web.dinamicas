1cc3c2a5dbd126aac49c920ed490f4938e4a7b9e
SantiagoAmoruso <amoruso.santiago@tecnica7.edu.ar>
2025-05-27 21:56:33 -0300

    Primer punto hecho

--- PHP-Mini.sitio.personal/index.php
+++ PHP-Mini.sitio.personal/index.php
@@ -1,3 +1,43 @@
 <?php
-    
+// - - Ejercicio: Variables, echo, condicionales y var_dump
+
+// 1. Declaramos las variables con datos personales
+$nombre = "Giliberta Anacarda";
+$edad = 14;
+$ciudad = "Córdoba";
+$estadoCivil = "soltera";
+
+// 2. Mostramos una frase con esos datos
+echo "Hola, soy $nombre, tengo $edad años, vivo en $ciudad y estoy $estadoCivil.<br>";
+
+// 3. Condicional para mostrar mensaje según la edad
+if ($edad < 18) {
+    echo "Soy menor de edad.<br>";
+}
+else
+if($edad <= 60) {
+    echo "Soy adulto.<br>";
+}
+else{
+    echo "Soy adulto mayor.<br>";
+}
+
+// 4. Mostramos el tipo de todas las variables con var_dump()
+echo "<br>Tipos de variables:<br>";
+
+var_dump($nombre);  // string
+echo "<br>";
+var_dump($edad);    // int
+echo "<br>";
+var_dump($ciudad);  // string
+echo "<br>";
+var_dump($estadoCivil); // string
+echo "<br><br>";
+
+$intereses = ["Snowboarding", "Teatro", "Tejer", "Dibujar", "Skiar"];
+
+echo "Intereses:<br>";
+foreach($intereses){
+    echo $intereses;
+}
 ?>
